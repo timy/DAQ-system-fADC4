8adbb909fa7649297bcb2310f21e4d309e4a7ba5
timy <timy.tech@gmail.com>
2023-01-10 09:57:10 +1030

    Bug fix: prevent buttons from flickering or even missing after running for a while

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow
     MainWindow win;
 
     if (!win.Create(L"Ndigo fast-ADC 4/10 Field Control", 
-        /*WS_OVERLAPPEDWINDOW */ WS_CAPTION | WS_VSCROLL, 0,
+        /*WS_OVERLAPPEDWINDOW */ WS_CAPTION | WS_VSCROLL | WS_CLIPCHILDREN, 0,
         CW_USEDEFAULT, CW_USEDEFAULT, 1150, 650)) {
         return 0;
     }
